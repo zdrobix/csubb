50248af1a6182ad8edb5d8dee4a5d7aedebfb618
zdrobix <zdroba88@gmail.com>
2024-10-25 12:37:51 +0300

    Lab5 trying to save users in database.
    Implemented the database connection code, resolving all exceptions, but
    I think I will circle back to the idea of extending the
    "InMemoryRepository", and then work on the memory data, not the
    database.
    
    Because of the incapability to inherit from multiple classes, I thought
    of making a static function that establishes connection to the database,
    But now when I think about it, with the new idea of extending
    "InMemoryRepository", I only need to establish the connection 2 times,
    when loading and when saving.
    
    I am very confused.

--- Semestru 3/MAP/lab/lab3/lab3/src/Repo/Repository.java
+++ Semestru 3/MAP/lab/lab3/lab3/src/Repo/Repository.java
@@ -2,20 +2,22 @@ package repo;
 
 import domain.Entity;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Optional;
 
 public interface Repository<ID, E extends Entity<ID>> {
 
-    Optional<E> findOne(ID id);
+    Optional<E> findOne(ID id) throws SQLException, IOException;
 
-    Iterable<E> findAll();
+    Iterable<E> findAll() throws SQLException;
 
-    Optional<E> save(E entity);
+    Optional<E> save(E entity) throws SQLException;
 
 
-    Optional<E> delete(ID id);
+    Optional<E> delete(ID id) throws SQLException;
 
-    Optional<E> update(E entity);
+    Optional<E> update(E entity) throws SQLException;
 
 }
 
--- Semestru 3/MAP/lab/lab3/lab3/src/Repo/db/AbstractDatabaseRepository.java
+++ Semestru 3/MAP/lab/lab3/lab3/src/Repo/db/AbstractDatabaseRepository.java
@@ -2,6 +2,7 @@ package repo.db;
 
 import domain.Entity;
 import domain.validators.Validator;
+import repo.Repository;
 import repo.memory.InMemoryRepository;
 
 import java.io.FileWriter;
@@ -10,53 +11,78 @@ import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-public abstract class AbstractDatabaseRepository<ID, E extends Entity<ID>> extends InMemoryRepository<ID, E> {
-    private Connection connection;
+public abstract class AbstractDatabaseRepository<ID, E extends Entity<ID>> implements Repository<ID, E> {
+
+    private List<String> connectionCredentials;
     private static String logFilename = "Q:\\info\\csubb\\Semestru 3\\MAP\\lab\\lab3\\lab3\\src\\logs\\logs.txt";
 
-    public AbstractDatabaseRepository(String url, String username, String password, Validator<E> validator) throws SQLException, IOException {
-        super(validator);
-        this.establishConnection(url, username, password);
+    public AbstractDatabaseRepository(String url, String username, String password, Validator<E> validator)  {
+        this.connectionCredentials = Arrays.asList(url, username, password);
     }
 
-    private void establishConnection(String url, String username, String password) throws SQLException, IOException {
-        var fileWriter = new FileWriter(logFilename, true);
+    public Optional<E> findOne(ID id) throws SQLException, IOException {
+        boolean connected = false;
+        Optional<E> result = Optional.empty();
         try {
-            this.connection = DriverManager.getConnection(url, username, password);
-            if (this.connection != null) {
-                fileWriter.append("Successfully connected to the database. " + LocalDateTime.now());
-            }
-            this.loadDatabase();
+            var connection = DriverManager
+                    .getConnection(
+                            this.connectionCredentials.get(0),
+                            this.connectionCredentials.get(1),
+                            this.connectionCredentials.get(2)
+                    );
+            var resultSet = connection
+                    .prepareStatement("SELECT * FROM USERS WHERE ID = ?")
+                    .executeQuery();
+            connected = true;
+            /*
+            De implementat.
+            Returneaza o enitate creata pe baza resultSet-ului.
+             */
+            connection.close();
         } catch (SQLException e) {
-            fileWriter.append("Failed to connect to the database. " + LocalDateTime.now());
+           connected = false;
         } finally {
-            fileWriter.close();
+            try {
+                FileWriter fw = new FileWriter(logFilename, true);
+                if (connected)
+                    fw.append("Connection established. " + LocalDateTime.now());
+                else
+                    fw.append("Connection failed. " + LocalDateTime.now());
+
+            } catch (IOException ie) {
+                /*
+                De implementat.
+                OK
+                 */
+            }
+            return result;
         }
     }
 
-    private void loadDatabase() throws SQLException {
-        var result = this.connection
-                        .prepareStatement(
-                                "SELECT * FROM ")
-                        .executeQuery();
-        while (result.next()) {
-            super.save(
-                    this.extractEntity(
-                            Arrays.asList(
-                                    result.getString(0),
-                                    result.getString(1),
-                                    result.getString(2)
-                            )
-                    )
-            );
-        }
-        this.connection.close();
+    public Iterable<E> findAll() {
+        return null;
+    }
+
+    public Optional<E> save(E entity) {
+        return null;
+    }
+
+
+    public Optional<E> delete(ID id) {
+        return null;
+    }
+
+    public Optional<E> update(E entity) {
+        return null;
     }
 
 
 
-    public abstract E extractEntity(List<String> attributes);
+
+
+
 }
 
 
