d42c3ef6ec9ec1ee8d8472eac6bc93e99b6d7e0d
zdrobix <zdroba88@gmail.com>
2024-10-26 13:22:56 +0300

    Started lab2 for plf. 10A is done, 10b remaining.

---
+++ Semestru 3/PLF/lab/lab2/lab2.pl
@@ -0,0 +1,99 @@
+%Problema 10
+
+/*
+ * Functie auxiliara: verifica daca numarul primit este o putere de 2
+ *
+ * putere2(N: intreg)
+ *
+ * N: numarul pentru care verificam daca este putere de 2
+ *
+ * model matematic:
+ *
+ * putere2(N) :  true                 - daca n = 1
+                 false                - daca n % 2 = 1
+ *               true si putere2(N/2) - daca n % 2 = 0
+ *
+ */
+
+putere2(1):-!.
+putere2(N):-
+    N > 1,
+    0 is N mod 2,
+    N2 is N // 2,
+    putere2(N2).
+putere2(N):-
+    N > 1,
+    1 is N mod 2,
+    false.
+
+test_putere():-
+    assertion(putere2(1)),
+    assertion(putere2(2)),
+    assertion(putere2(4)),
+    assertion(putere2(8)),
+    assertion(putere2(16)),
+    assertion(not(putere2(0))),
+    assertion(not(putere2(3))),
+    assertion(not(putere2(7))),
+    assertion(not(putere2(10))),
+    assertion(not(putere2(12))).
+
+
+
+/*
+ * 10a) Se da o lista de numere intregi. Se cere sa se adauge in lista
+ * dupa primul element, al 3-lea element, al 7-lea element, al 15-lea
+ * element .. o valoare data e.
+ *
+ * adaugaLista(L: lista, E: element, P: intreg, R: lista)
+ *
+ * model de flux (i, i) sau (o, i)
+ * L: lista in care se doreste inserarea lui e pe pozitiile 1,3,7,15..
+ * E: elementul care se doreste a fi inserat in lista L
+ * P: un intreg auxiliar cu care se determina pozitia unde se insereaza
+ * R: lista finala dupa inserarea elementului E pe pozitiile aferente
+ *
+ * model matematic:
+ *
+ * adaugaLista (L1..Ln, E, P) =
+ *          []                                      - daca n = 0
+ *          L1 U E U adaugaLista(L2..Ln, E, P + 1)  - daca P = 2^k - 1
+ *          L1 U adaugaLista(L2..Ln, E, P + 1)      - daca P/= 2^k - 1
+ *
+ *          cu k > 0
+ */
+
+
+adaugaLista([], _, _, []):-!.
+adaugaLista([H|T], E, P, [H, E|R]):-
+    P1 is P + 1,
+    putere2(P1),
+    adaugaLista(T, E, P1, R).
+adaugaLista([H|T], E, P, [H|R]):-
+    adaugaLista(T, E, P + 1, R).
+
+test_adaugaLista():-
+    assertion(adaugaLista([], 1, 1, [])),
+    assertion(adaugaLista([1, 3], 2, 1, [1, 2, 3])),
+    assertion(adaugaLista([1, 2, 3], 8, 1, [1, 8, 2, 3, 8])),
+    adaugaLista([], 1, 1, L1),
+    adaugaLista([1, 3], 2, 1, L2),
+    adaugaLista([1, 2, 3], 8, 1, L3),
+    assertion(L1 = []),
+    assertion(L2 = [1, 2, 3]),
+    assertion(L3 = [1, 8, 2, 3, 8]).
+
+/*
+ * 10b) Se da o lista eterogena, formata din numere intregi si liste de
+ * numere intregi. Lista incepe cu un numar si nu sunt 2 elemente
+ * consecutive care sunt liste. Se cere ca in fiecare sublista sa se
+ * adauge dupa primul, al 3-lea, al 7-lea element valoarea care se
+ * gaseste inainte de sublista in lista eterogena.
+ *
+ * adaugaListaEterogena(L: lista, E: intreg,
+ *
+ *
+ *
+ */
+
+
