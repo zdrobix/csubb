e9bab8087d33c6e93aa4f3bbaa537fb2a509105e
zdrobix <zdroba88@gmail.com>
2024-11-23 19:05:57 +0200

    Started lab4 for PLF. LISP: remaining problem 12.d

---
+++ Semestru 3/PLF/lab/lab4/lab4.lisp
@@ -0,0 +1,76 @@
+;12.a) functie care intoarce produsul scalar a doi vectori
+;v1 v2 - vectori
+;daca vectorii nu au aceasi lungime -> eroare
+(defun produs_scalar(v1 v2)
+  	(if (/= (length v1) (length v2))
+    		(error "Format invalid!")
+    		(reduce #'+ 
+			(mapcar #'* v1 v2)
+		)
+	)
+)
+
+
+;12.b) maximul atomilor numerici dintr-o lista, de la orice nivel
+;model matematic 
+; maxim(l1..ln) = 0                            - daca n = 0
+;                 max(l1, maxim(l2..ln)        - daca l1 atom numeric
+; 	          maxim(l2..ln)                - daca l1 este atom
+;                 max(maxim(l1), maxim(l2..ln) - altfel
+(defun maxim_lista(lista)
+	(if (null lista)
+	  0
+	  (if (listp (car lista))
+		     (max 
+		      		(maxim_lista 
+				  	(car lista)
+				)
+			   	(maxim_lista 
+				  	(cdr lista)
+				)
+			)
+		      (max 
+				(car lista)
+			   	(maxim_lista 
+				  	(cdr lista)
+				)
+			)
+			)
+	  )
+)
+
+
+;12.c) o functie care intoarce lista permutarilor unei liste date
+;model matematic
+; permutari(l1..ln) = []                          - daca n = 0
+; 		      [l1 U permutari(l2..ln)] U  
+;                     permutari(l2..ln)           - daca n > 0
+(defun permutari(lista)
+  	(if (null lista)
+		'(())
+		(append 
+		  	(insert-all 
+			  	(car lista)
+				(cdr lista)
+			)
+			(permutari
+			  	(cdr lista)
+			)
+		)
+	)
+)
+
+;insereaza elementul x pe fiecare pozitie din lista lista
+;model matematic
+; insert-all(x, l1..ln) = (x)                                - daca n = 0
+;                         [xl1..ln] U insert-all(x, l2..ln)  - daca n > 0
+(defun insert-all (x lista) 
+  	(if (null lista) 
+	  	(list (list x))
+		(cons (cons x lista)
+		      	(insert-all x (cdr lista)))))
+		
+
+
+;12.d) o functie care intoarce T daca o lista are nr par de elem, si NIL in caz contrar. fara sa se numere elementele listei
+
