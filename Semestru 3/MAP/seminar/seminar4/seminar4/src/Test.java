1722e65c0a4433ee0ae0081ca3c2529ef7591e81
zdrobix <zdroba88@gmail.com>
2025-01-30 18:03:58 +0200

    Created a test case.

--- Semestru 3/MAP/seminar/seminar4/seminar4/src/Test.java
+++ Semestru 3/MAP/seminar/seminar4/seminar4/src/Test.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.stream.Stream;
 
-public class Test {
-}
+class R12 {
+    public static void main(String[] args) {
+        Stream<String> ss = Stream.of("asd ","bus ","aop ");
+        var res = ss
+                .filter(s -> {
+                    System.out.print(s);
+                    return s.contains("a");
+                })
+                .map((x) ->
+                {
+                    System.out.print(x);
+                    return x.toUpperCase();
+                })
+                .reduce("", (x, y) -> x + y);
+        System.out.println(res);
+    }}
