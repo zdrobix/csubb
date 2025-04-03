7b9a2751f927f5898113fe18efeb3fd3d9c8c712
zdrobix <zdroba88@gmail.com>
2025-04-03 14:59:19 +0300

    Finished lab4.

--- Semestru 4/IA/lab/lab4/main.py
+++ Semestru 4/IA/lab/lab4/main.py
@@ -10,6 +10,8 @@ from PIL import Image
 from PIL import ImageColor
 from PIL import ImageDraw
 
+from shapely.geometry import box
+
 import datetime
 import os
 
@@ -18,6 +20,20 @@ module_handle = "https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_re
 detector = hub.load(module_handle).signatures['default']
 min_score = .1
 
+bike_coords = {
+    "./images/bikes/bike1.jpg": [(8, 20, 400, 408)],
+    "./images/bikes/bike02.jpg": [(30, 83, 387, 323)],
+    "./images/bikes/bike03.jpg": [(76, 144, 198, 388), (167, 151, 342, 394)],
+    "./images/bikes/bike04.jpg": [(5, 4, 412, 412)],
+    "./images/bikes/bike05.jpg": [(74, 57, 353, 342)],
+    "./images/bikes/bike06.jpg": [(76, 144, 198, 388), (167, 151, 342, 394)],
+    "./images/bikes/bike07.jpg": [(64, 217, 295, 414)],
+    "./images/bikes/bike08.jpg": [(55, 39, 380, 350)],
+    "./images/bikes/bike09.jpg": [(18, 14, 376, 387)],
+    "./images/bikes/bike10.jpg": [(149, 138, 371, 405)],
+}
+
+# 15 minute pentru a localiza bicicletele manual
 
 def display_image(image):
     fig = plt.figure(figsize=(20, 15))
@@ -33,15 +49,27 @@ def load_image(image_path):
     return img
 
 
-def draw_on_image(image, y1, x1, y2, x2):
-    draw = ImageDraw.Draw(image)
+def get_iou(rectangle1, rectangle2):
+    inter = rectangle1.intersection(rectangle2).area
+    union = rectangle1.union(rectangle2).area
+    if union == 0:
+        return 0
+    return inter / union
+
+
+def convert_coords(image, y1, x1, y2, x2):
     image_width, image_height = image.size
-    (left, right, top, bottom) = (x1 * image_width, x2 * image_width,
-                                  y1 * image_height, y2 * image_height)
+    return y1 * image_height, x1 * image_width, y2 * image_height, x2 * image_width
+
+def draw_on_image(image, y1, x1, y2, x2, color, convert):
+    draw = ImageDraw.Draw(image)
+    left, right, top, bottom = x1, x2, y1, y2
+    if convert:
+        (top, left, bottom, right) = convert_coords(image, y1, x1, y2, x2)
     draw.line([(left, top), (left, bottom), (right, bottom), (right, top),
                (left, top)],
               width=1,
-              fill=ImageColor.getrgb("red"))
+              fill=ImageColor.getrgb(color))
 
 
 def draw_boxes(image, detected_data):
@@ -49,20 +77,30 @@ def draw_boxes(image, detected_data):
                             detected_data["detection_scores"])
 
     other_objects = []
-    time_difference = 0
+    image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
+    actual_bike_coords = []
 
     for i in range(min(boxes.shape[0], 5)):
         if names[i].decode("utf-8") in ["Bicycle", "bicycle", "Bike", "bike"]:
             if scores[i] > min_score:
                 y1, x1, y2, x2 = tuple(boxes[i])
-                image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
+                actual_bike_coords += [convert_coords(image_pil, y1, x1, y2, x2)]
                 print("Score: " + f"{int(100 * scores[i])}%")
                 draw_on_image(
                     image_pil,
-                    y1, x1, y2, x2)
+                    y1, x1, y2, x2, "red", True)
                 np.copyto(image, np.array(image_pil, dtype=image.dtype))
+
         else:
             other_objects += [f"{names[i].decode('utf-8')}"]
+
+    for boxx in bike_coords["./images/bikes/bike1.jpg"]:
+        y1, x1, y2, x2 = tuple(boxx)
+        draw_on_image(image_pil, y1, x1, y2, x2, "green", False)
+        for actual_boxx in actual_bike_coords:
+            iou = get_iou(box(*boxx), box(*actual_boxx))
+            print(f"Iou: {iou}")
+        np.copyto(image, np.array(image_pil, dtype=image.dtype))
     print("Other objects detected:  " + ", ".join(other_objects))
     return image
 
@@ -76,7 +114,8 @@ def detect(detector_, path):
     result = {key: value.numpy() for key, value in result.items()}
     boxed_image = draw_boxes(img.numpy(), result)
     display_image(boxed_image)
-    print(f"Time for finding the bike: {end_time - start_time}")
+    print(f"Model time/Human time: {end_time - start_time} / 00:01:30.00000")
+
 
 def detect_if_bike(detector_, path):
     img = load_image(path)
@@ -90,7 +129,5 @@ def detect_if_bike(detector_, path):
 
 
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
-# print(detect_if_bike(detector, "./images/animals.png"))
-# print(detect_if_bike(detector, "./images/bikes/bike1.jpg"))
-detect(detector, "./images/bikes/bike05.jpg")
 
+detect(detector, "./images/bikes/bike1.jpg")
