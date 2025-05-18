d4cdec2acb0aafdcef87c9363eb38350abd8b99e
zdrobix <zdroba88@gmail.com>
2025-05-18 15:13:42 +0300

    Added lab4 SGBD, second problem, also added logging.

---
+++ Semestru 4/SGBD/lab/lab4/deadlock/deadlock/Program.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace deadlock
+{
+	public class Application 
+	{
+		private static string ConnectionString = "Server=DESKTOP-9PS0RE2;Database=zdrobix;Trusted_Connection=True;TrustServerCertificate=True;";
+		private const int MaxRetryCount = 3;
+
+		public static void Main(string[] args)
+		{
+			var task1 = Task.Run(() => Transaction1());
+			var task2 = Task.Run(() => Transaction2());
+
+			Task.WaitAll(task1, task2);
+			Console.WriteLine("Done.");
+		}
+
+		static void Transaction1()
+		{
+			int attempt = 0;
+			while (attempt <  MaxRetryCount) 
+			{
+				try
+				{
+					using (var connection = new SqlConnection(ConnectionString))
+					{
+						connection.Open();
+
+						var transaction = connection.BeginTransaction();
+
+						var command1 = new SqlCommand(
+							"UPDATE CLIENTI SET nume='Deadlock Client 2' " +
+							"WHERE nume= 'Deadlock Client 1'", connection, transaction
+						);
+						command1.ExecuteNonQuery();
+						Console.WriteLine("Transaction1: Updated CLIENTI.");
+						Thread.Sleep(5000);
+
+						var command2 = new SqlCommand(
+							"UPDATE BENEFICII SET nume='Deadlock Beneficiu 2' " +
+							"WHERE nume= 'Deadlock Beneficiu 1'", connection, transaction
+						);
+						command2.ExecuteNonQuery();
+
+						transaction.Commit();
+						Console.WriteLine("Transaction1: Committed.");
+						break;
+					}
+				}
+				catch (SqlException ex) when (ex.Number == 1205)
+				{
+					attempt++;
+					Console.WriteLine($"Transaction1: Deadlock. Retry {attempt}/{MaxRetryCount}");
+					Thread.Sleep(500);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("Transaction 1 failed: " + ex.Message);
+				}
+			}
+			if (attempt == MaxRetryCount)
+				Console.WriteLine("Aborted.");
+		}
+
+		static void Transaction2()
+		{
+			int attempt = 0;
+			while (attempt < MaxRetryCount)
+			{
+				try
+				{
+					using (var connection = new SqlConnection(ConnectionString))
+					{
+						connection.Open();
+
+						var transaction = connection.BeginTransaction();
+
+						var command1 = new SqlCommand(
+							"UPDATE BENEFICII SET nume='Deadlock Beneficiu 3' " +
+							"WHERE nume= 'Deadlock Beneficiu 1'", connection, transaction
+						);
+						command1.ExecuteNonQuery();
+						Console.WriteLine("Transaction2: Updated BENEFICII.");
+						Thread.Sleep(5000);
+
+						var command2 = new SqlCommand(
+							"UPDATE CLIENTI SET nume='Deadlock Client 3' " +
+							"WHERE nume= 'Deadlock Client 1'", connection, transaction
+						);
+						command2.ExecuteNonQuery();
+
+						transaction.Commit();
+						Console.WriteLine("Transaction2: Committed.");
+						break;
+					}
+				}
+				catch (SqlException ex) when (ex.Number == 1205)
+				{
+					attempt++;
+					Console.WriteLine($"Transaction2: Deadlock. Retry {attempt}/{MaxRetryCount}");
+					Thread.Sleep(500);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("Transaction 2 failed: " + ex.Message);
+				}
+			}
+			if (attempt == MaxRetryCount)
+				Console.WriteLine("Aborted.");
+		}
+	}
+}
--- Semestru 4/SGBD/lab/lab4/problem1.sql
+++ Semestru 4/SGBD/lab/lab4/problem1.sql
@@ -6,6 +6,7 @@ BEGIN TRANSACTION
 UPDATE BENEFICII 
 SET nume='Reduce febra'
 WHERE id = 1
+INSERT INTO _Log(tableName, typeOp, dateOp) VALUES ('BENEFICII', 'UPDATE', GETDATE());
 WAITFOR DELAY '00:00:05'
 ROLLBACK TRANSACTION
 
@@ -27,11 +28,13 @@ where nume = 'Energie sporita si chef de viata'
 
 INSERT INTO BENEFICII (nume)
 VALUES ('Energie sporita')
+INSERT INTO _Log(tableName, typeOp, dateOp) VALUES ('BENEFICII', 'INSERT', GETDATE());
 BEGIN TRAN
 WAITFOR DELAY '00:00:05'
 UPDATE BENEFICII
 SET nume='Energie sporita si chef de viata'
 WHERE nume='Energie sporita'
+INSERT INTO _Log(tableName, typeOp, dateOp) VALUES ('BENEFICII', 'UPDATE', GETDATE());
 COMMIT TRAN
 
 SET TRANSACTION ISOLATION LEVEL READ COMMITTED
@@ -51,6 +54,7 @@ BEGIN TRAN
 WAITFOR DELAY '00:00:05'
 INSERT INTO BENEFICII (nume)
 VALUES ('/////////////')
+INSERT INTO _Log(tableName, typeOp, dateOp) VALUES ('BENEFICII', 'INSERT', GETDATE());
 COMMIT TRAN
 
 SET TRANSACTION ISOLATION LEVEL REPEATABLE READ
@@ -71,21 +75,27 @@ INSERT INTO CLIENTI(nume, varsta)
 VALUES ('Petrica Georgica 5', 50)
 INSERT INTO BENEFICII(nume)
 VALUES ('Vedere imbunatatita 5')
+INSERT INTO _Log(tableName, typeOp, dateOp) VALUES ('CLIENTI', 'INSERT', GETDATE());
+INSERT INTO _Log(tableName, typeOp, dateOp) VALUES ('BENEFICII', 'INSERT', GETDATE());
 
 BEGIN TRAN
 UPDATE CLIENTI SET nume='Petrica Georgica 6'
 WHERE nume= 'Petrica Georgica 5'
+INSERT INTO _Log(tableName, typeOp, dateOp) VALUES ('CLIENTI', 'UPDATE', GETDATE());
 WAITFOR DELAY '00:00:05'
 UPDATE BENEFICII SET nume= 'Vedere imbunatatita 6'
 WHERE nume= 'Vedere imbunatatita 5'
+INSERT INTO _Log(tableName, typeOp, dateOp) VALUES ('BENEFICII', 'UPDATE', GETDATE());
 COMMIT TRAN
 
 BEGIN TRAN
 UPDATE BENEFICII SET nume= 'Vedere imbunatatita 7'
 WHERE nume= 'Vedere imbunatatita 5'
+INSERT INTO _Log(tableName, typeOp, dateOp) VALUES ('BENEFICII', 'UPDATE', GETDATE());
 WAITFOR DELAY '00:00:05'
 UPDATE CLIENTI SET nume='Petrica Georgescu 7'
 WHERE nume= 'Petrica Georgica 5'
+INSERT INTO _Log(tableName, typeOp, dateOp) VALUES ('CLIENTI', 'UPDATE', GETDATE());
 COMMIT TRAN
 
 SELECT * FROM CLIENTI
@@ -94,3 +104,15 @@ SELECT * FROM BENEFICII
 -- Solution ->
 -- set priorities for transaction
 -- update the tables in the same order to avoid locking resources at the same time
+
+
+
+----------------------------------------------------
+
+DELETE FROM CLIENTI WHERE nume LIKE 'Deadlock%'
+DELETE FROM BENEFICII WHERE nume LIKE 'Deadlock%'
+
+INSERT INTO CLIENTI(nume, varsta) 
+VALUES ('Deadlock Client 1', 50)
+INSERT INTO BENEFICII(nume)
+VALUES ('Deadlock Beneficiu 1')
