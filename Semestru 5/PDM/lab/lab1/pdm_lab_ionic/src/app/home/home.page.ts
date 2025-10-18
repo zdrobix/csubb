035036a542428e552883bf2e11e3c2e15fb66d99
zdrobix <zdroba88@gmail.com>
2025-10-18 10:35:09 +0300

    Added master-detail user interface.

---
+++ Semestru 5/PDM/lab/lab1/pdm_lab_ionic/src/app/home/home.page.ts
@@ -0,0 +1,53 @@
+import { Component, Injectable, OnDestroy, OnInit } from '@angular/core';
+import { Car } from '../models/Car';
+import { Observable, of, Subscription } from 'rxjs';
+import { CarService } from '../services/car-service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+@Injectable({
+  providedIn: 'root'
+})
+export class HomePage implements OnInit, OnDestroy {
+
+  data: any
+  cars$?: Observable<Car[]>;
+  getAllSubscription?: Subscription;
+
+  selectedCar$?: Observable<Car>;
+
+  constructor(private carService: CarService ) {}
+
+  ngOnInit() {
+    this.getAllSubscription = this.carService.getAllCars().subscribe(result => {
+      this.data = result;
+      const cars: Car[] = (this.data.data ?? []).map(
+        (element: { 
+          id: any; 
+          name: any; 
+          registration_number: any; 
+          accident_count: any; 
+        }) => ({
+          id: parseInt(element.id, 10), 
+          name: element.name, 
+          registration_number: element.registration_number, 
+          accident_count: element.accident_count
+        })
+      );
+      this.cars$ = of(cars)
+    });
+  }
+
+  setInfoCar(car: any) {
+    this.selectedCar$ = of(car);
+  }
+
+  ngOnDestroy() {
+    this.getAllSubscription?.unsubscribe();
+  }
+
+}
