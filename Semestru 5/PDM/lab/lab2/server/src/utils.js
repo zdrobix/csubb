45e99cdbc4dfa6a72d3935549a624c3e9e623e3e
zdrobix <zdroba88@gmail.com>
2025-11-02 23:46:29 +0200

    Added jwt authorization.

--- Semestru 5/PDM/lab/lab2/server/src/utils.js
+++ Semestru 5/PDM/lab/lab2/server/src/utils.js
@@ -1,2 +1,28 @@
+import jwt from 'jsonwebtoken';
+
 export const jwtConfiguration = { secret: 'my-secret'};
 
+export const jwtAuth = async (ctx, next) => {
+    const authHeader = ctx.headers['authorization'];
+    if (!authHeader) {
+        ctx.status = 401;
+        ctx.body = { message: 'Authorization header missing' };
+        return;
+    }
+
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+        ctx.status = 401;
+        ctx.body = { message: 'Token missing' };
+        return;
+    }
+
+    try {
+        const decoded = jwt.verify(token, jwtConfiguration.secret);
+        ctx.state.user = decoded;
+        await next();
+    } catch (err) {
+        ctx.status = 403;
+        ctx.body = { message: 'Invalid token' };
+    }
+};
